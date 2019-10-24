20eed9fd6621e5d867a901d4ae90be91ee03cb14
at1208 <amantiwari12081999@gmail.com>
2019-10-24 18:46:15 +0530

    login design created

--- client/src/components/Login/login.js
+++ client/src/components/Login/login.js
@@ -6,15 +6,20 @@ import Header from '../header'
 const Login  = () => {
   return <div className='container-fluid'>
   <Header />
-  <div className='card container b1 col-md-4'>
-         <h1 className='text-center'>LOGIN</h1>
-          <label>Username</label>
+  <div className='card container b1 col-md-3 shadow'>
+          <h1 className='text-center b5'>Cheetah</h1>
+          <br />
+          <span className='text-center b4'>Please Enter Your Information</span>
+          <hr />
+
           <input className='b3 text-center' placeholder='Please Enter your Username'/>
 
-          <label>Password</label>
           <input className='b3 text-center'placeholder='Please Enter your Password'/>
 
-          <Link to='/dashboard'><button className='btn btn-block b2 '>Submit</button></Link>
+          <Link to='/dashboard'><button className='btn btn-block b2 '>Login to Cheetah</button></Link>
+
+          <button className='btn btn-block b6'>I forgot my password/ Reset password</button>
+
   </div>
 </div>
 }
